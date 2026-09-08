19953af6d61e05d70e11f86a98c7ef953d40f567
OliviaQiu779 <rq2032@nyu.edu>
2026-09-08 15:58:26 -0400

    Add weather loop practice

--- Week 1/MyPlayground.playground/Contents.swift
+++ Week 1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//var greeting = "Hello, playground"
+
+//let weather = ["☀️", "🌤️", "☁️", "🌨️", "❄️", "🌧️"]
+//
+//func checkWeather(_ weather: String) -> String {
+//    if weather == "🌧️" {
+//        return "Bring an umbrella!"
+//    } else {
+//        return "It's a \(weather) day!"
+//    }
+//}
+//
+//var today = ""
+//
+//while today != "🌧️" {
+//    today = weather.randomElement()!
+//    print(checkWeather(today))
+//}
+
+let weather = ["☀️", "🌤️", "☁️", "🌨️", "❄️", "🌧️"]
+
+func checkWeather(weather: String) {
+    if weather == "🌧️" {
+        print("Bring an umbrella!")
+    } else {
+        print("It's a \(weather) day!")
+    }
+}
+
+var today = ""
+
+while today != "🌧️" {
+    today = weather.randomElement()!
+    checkWeather(weather: today)
+}
